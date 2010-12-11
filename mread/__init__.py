98fff48ce2643fe1319b9f9658ba2e662897eed0
Alexander Tchekhovskoy <atchekho@princeton.edu>
2010-12-11 14:32:50 -0500

    update

--- mread/__init__.py
+++ mread/__init__.py
@@ -19,20 +19,82 @@ from matplotlib import cm
 from numpy import ma
 import matplotlib.colors as colors
 import os,glob
+import pylab
 
 
 #global rho, ug, vu, uu, B, CS
-#global nx,ny,nz,_dx1,_dx2_,_dx3,ti,tj,tk,x1,x2,x3,r,h,ph,gdet,conn,gn3,gv3,ck,dxdxp
+#global nx,ny,nz,_dx1,_dx2,_dx3,ti,tj,tk,x1,x2,x3,r,h,ph,gdet,conn,gn3,gv3,ck,dxdxp
 
-def plc(myvar): #plc
-    global x1, x2
-    res = plt.contour(x1[:,:,0],x2[:,:,0],myvar[:,:,0],15)
-    plt.colorbar()
+def Qmri():
+    """
+    Computes number of theta cells resolving one MIR wavelength
+    """
+
+def plc(myvar,**kwargs): #plc
+    xcoord = kwargs.pop('x1', None)
+    ycoord = kwargs.pop('x2', None)
+    cb = kwargs.pop('cb', False)
+    if( xcoord == None or ycoord == None ):
+        res = plt.contour(myvar[:,:,0].transpose(),15,**kwargs)
+    else:
+        res = plt.contour(xcoord[:,:,0],ycoord[:,:,0],myvar[:,:,0],15,**kwargs)
+    if( cb == True): #use color bar
+        plt.colorbar()
+
+def reinterp(vartointerp,extent,ncell):
+    global xi,yi,zi
+    #grid3d("gdump")
+    #rfd("fieldline0250.bin")
+    xraw=r*np.sin(h)
+    yraw=r*np.cos(h)
+    x=xraw[:,:,0].view().reshape(-1)
+    y=yraw[:,:,0].view().reshape(-1)
+    var=vartointerp[:,:,0].view().reshape(-1)
+    #mirror
+    x=np.concatenate((-x,x))
+    y=np.concatenate((y,y))
+    var=np.concatenate((vartointerp[:,:,0].view().reshape(-1),var))
+    # define grid.
+    xi = np.linspace(extent[0], extent[1], ncell)
+    yi = np.linspace(extent[2], extent[3], ncell)
+    # grid the data.
+    zi = griddata((x, y), var, (xi[None,:], yi[:,None]), method='linear')
+    interior = np.sqrt((xi[None,:]**2) + (yi[:,None]**2)) < 1+np.sqrt(1-a**2)
+    #zi[interior] = np.ma.masked
+    varinterpolated = ma.masked_where(interior, zi)
+    return(varinterpolated)
+    
+def mkframe(fname,vmin=None,vmax=None):
+    len = 60
+    extent=(-len,len,-len,len)
+    ncell=800
+    palette=cm.jet
+    palette.set_bad('k', 1.0)
+    palette.set_over('r', 1.0)
+    palette.set_under('g', 1.0)
+    aphi = fieldcalc()
+    iaphi = reinterp(aphi,extent,ncell)
+    ilrho = reinterp(np.log10(rho),extent,ncell)
+    #maxabsiaphi=np.max(np.abs(iaphi))
+    maxabsiaphi=6
+    levs=np.linspace(-maxabsiaphi,maxabsiaphi,128)
+    cset2 = plt.contour(iaphi,linewidths=0.5,colors='k', extent=extent,hold='on',origin='lower',levels=levs)
+    #for c in cset2.collections:
+    #    c.set_linestyle('solid')
+    #CS = plt.contourf(xi,yi,zi,15,cmap=palette)
+    CS = plt.imshow(ilrho, extent=extent, cmap = palette, norm = colors.Normalize(clip = False),origin='lower',vmin=vmin,vmax=vmax)
+    #CS.cmap=cm.jet
+    #CS.set_axis_bgcolor("#bdb76b")
+    plt.colorbar(CS) # draw colorbar
+    plt.xlim(extent[0],extent[1])
+    plt.ylim(extent[2],extent[3])
+    plt.title(r'$\log_{10}\rho$ at $t = %4.0f$' % t)
+    plt.savefig( fname + '.png' )
 
 def mainfunc(imgname):
     global xi,yi,zi,CS
     #grid3d("gdump")
-    #jrdp3d("fieldline0250.bin")
+    #rfd("fieldline0250.bin")
     xraw=r*np.sin(h)
     yraw=r*np.cos(h)
     lrhoraw=np.log10(rho)
@@ -88,7 +150,88 @@ def mainfunc(imgname):
     # plt.title('Grid plot')
     # plt.show()
 
-def jrdp3d(fieldlinefilename):
+def rrdump(dumpname):
+    global nx,ny,nz,t,a,rho,ug,vu,vd,B,gd,numcols,gdetB
+    #print( "Reading " + "dumps/" + dumpname + " ..." )
+    gin = open( "dumps/" + dumpname, "rb" )
+    header = gin.readline().split()
+    nx = int(header[0])
+    ny = int(header[1])
+    nz = int(header[2])
+    t  = float(header[3])
+    a  = float(header[6])
+    gin.close()
+    gd1 = np.loadtxt( "dumps/" + dumpname, 
+                      dtype=float, 
+                      skiprows=1, 
+                      unpack = True )
+    gd=gd1.view().reshape((-1,nx,ny,nz), order='F')
+    rho,ug = gd[0:2,:,:,:].view() 
+    B = np.zeros_like(gd[4:8])
+    vu = np.zeros_like(B)
+    vu[1:4] = gd[2:5].view() #relative 4-velocity only has non-zero spatial components
+    B[1:4] = gd[5:8].view()
+    numcols = gd.shape[0]  #total number of columns is made up of (n prim vars) + (n cons vars) = numcols
+    gdetB = np.zeros_like(B)
+    gdetB[1:4] = gd[numcols/2+5:numcols/2+5+3]  #gdetB starts with 5th conserved variable
+    if 'gv3' in globals() and 'gn3' in globals(): 
+        vd = mdot(gv3,vu)
+        gamma = (1+mdot(mdot(gv3,vu),vu))**0.5
+        etad = sc.zeros_like(vu)
+        etad[0] = -1/(-gn3[0,0])**0.5      #ZAMO frame velocity (definition)
+        etau = mdot(gn3,etad)
+        uu = gamma * etau + vu
+        ud = mdot(gv3,uu)
+    else:
+        print( 'Metric (gv3, gn3) not defined, I am skipping the computation of uu and ud' )
+
+   
+def fieldcalcU():
+    """
+    Computes the field vector potential
+    """
+    daphi = mysum2(gdetB[1])*_dx2*_dx3
+    aphi=daphi.cumsum(axis=1)
+    aphi-=0.5*daphi #correction for half-cell shift between face and center in theta
+    aphi[0:nx-1] = 0.5*(aphi[0:nx-1]+aphi[1:nx]) #and in r
+    return(aphi)
+
+def rd(dump):
+    global t,nx,ny,nz,_dx1,_dx2,_dx3,gam,a,ti,tj,tk,x1,x2,x3,r,h,ph,rho,ug,vu,B,pg,cs2,Sden,U,divb,uu,ud,bu,bd
+    global v1m,v1p,v2m,v2p,v3m,v3p,bsq
+    #read image
+    fin = open( "dumps/" + dump, "rb" )
+    header = fin.readline().split()
+    t = np.float64(header[0])
+    nx = int(header[1])
+    ny = int(header[2])
+    nz = int(header[3])
+    _dx1=float(header[7])
+    _dx2=float(header[8])
+    _dx3=float(header[9])
+    gam=float(header[11])
+    a=float(header[12])
+    fin.close()
+    gd = np.loadtxt( "dumps/"+dump, 
+                      dtype=float, 
+                      skiprows=1, 
+                      unpack = True ).view().reshape((-1,nx,ny,nz), order='F')
+    ti,tj,tk,x1,x2,x3,r,h,ph,rho,ug = gd[0:11,:,:,:].view() 
+    vu=np.zeros_like(gd[0:4])
+    B=np.zeros_like(gd[0:4])
+    vu[1:4] = gd[11:14]
+    B[1:4] = gd[14:17]
+    pg,cs2,Sden = gd[17:20]
+    U = gd[20:29]
+    divb = gd[29]
+    uu = gd[30:34]
+    ud = gd[34:38]
+    bu = gd[38:42]
+    bd = gd[42:46]
+    bsq = mdot(bu,bd)
+    v1m,v1p,v2m,v2p,v3m,v3p=gd[46:52]
+
+def rfd(fieldlinefilename,**kwargs):
     global t,nx,ny,nz,_dx1,_dx2,_dx3,gam,a,rho,ug,uu,uut,uu,B,uux
     #read image
     fin = open( "dumps/" + fieldlinefilename, "rb" )
@@ -115,8 +258,26 @@ def jrdp3d(fieldlinefilename):
     #for i in range(1,4):
     #    uux[i,:,:,:] = uux[i,:,:,:] * uux[0,:,:,:]  
     uu[1:4]=uu[1:4] * uu[0]
+    useoldfmt = kwargs.pop('oldfmt',False)
+    #old image format
     B = sc.zeros_like(uu)
     B[1:4,:,:,:]=d[8:11,:,:,:]
+    if(useoldfmt == False): 
+        #new image format additionally contains gdet*B^i
+        gdetB = np.zeros_like(B)
+        gdetB[1:4] = d[8:11,:,:,:]
+    #     if 'gdet' in globals():
+    #         #first set everything approximately (B's are at shifted locations by half-cell)
+    #         B = gdetB/gdet  
+    #         #then, average the inner cells to proper locations
+    #         B[1,0:nx-1,:,:] = 0.5*(gdetB[1,0:nx-1,:,:]+gdetB[1,1:nx,:,:])/gdet[0:nx-1,:,:]
+    #         B[2,:,0:ny-1,:] = 0.5*(gdetB[2,:,0:ny-1,:]+gdetB[2,:,1:ny,:])/gdet[:,0:ny-1,:]
+    #         B[3,:,:,0:nz-1] = 0.5*(gdetB[3,:,:,0:nz-1]+gdetB[3,:,:,1:nz])/gdet[:,:,0:nz-1]
+    #         #note: last cells on the grids (near upper boundaries of each dir are at
+    #         #      approximate locations
+    #     else:
+    #         print( "rfd: warning: since gdet is not defined, I am skipping the computation of cell-centered fields, B" )
+    # else:
 
 
 def cvel():
@@ -138,7 +299,7 @@ def cvel():
 
 
 def grid3d(dumpname): #read gdump: header and body
-    global nx,ny,nz,_dx1,_dx2_,_dx3,ti,tj,tk,x1,x2,x3,r,h,ph,conn,gn3,gv3,ck,dxdxp,gdet
+    global nx,ny,nz,_dx1,_dx2,_dx3,ti,tj,tk,x1,x2,x3,r,h,ph,conn,gn3,gv3,ck,dxdxp,gdet
     print( "Reading grid from " + "dumps/" + dumpname + " ..." )
     gin = open( "dumps/" + dumpname, "rb" )
     header = gin.readline().split()
@@ -150,7 +311,7 @@ def grid3d(dumpname): #read gdump: header and body
     _dx3=float(header[9])
     gin.close()
     #read gdump
-    
+    #
     gd = np.loadtxt( "dumps/gdump", 
                       dtype=float, 
                       skiprows=1, 
@@ -169,6 +330,65 @@ def grid3d(dumpname): #read gdump: header and body
     ck = gd[106:110].view().reshape((4,nx,ny,nz), order='F')
     dxdxp = gd[110:136].view().reshape((4,4,nx,ny,nz), order='F').transpose(1,0,2,3,4)
     print( "Done!" )
+
+def grid3dlight(dumpname): #read gdump: header and body
+    global nx,ny,nz,_dx1,_dx2,_dx3,ti,tj,tk,x1,x2,x3,r,h,ph,conn,gn3,gv3,ck,dxdxp,gdet
+    print( "Reading grid from " + "dumps/" + dumpname + " ..." )
+    gin = open( "dumps/" + dumpname, "rb" )
+    header = gin.readline().split()
+    nx = int(header[1])
+    ny = int(header[2])
+    nz = int(header[3])
+    _dx1=float(header[7])
+    _dx2=float(header[8])
+    _dx3=float(header[9])
+    gin.close()
+    #read gdump
+    #
+    gd = np.loadtxt( "dumps/" + dumpname, 
+                      dtype=float, 
+                      skiprows=1, 
+                      unpack = True,
+                      usecols=(0,1,2,3,4,5,6,7,8,105)).view().reshape((-1,nx,ny,nz), order='F')
+    #gd = np.genfromtxt( "dumps/gdump", 
+    #                 dtype=float, 
+    #                 skip_header=1, 
+    #                 skip_footer=nx*ny*(nz-1),
+    #                 unpack = True ).view().reshape((137,nx,ny,nz), order='F')
+    ti,tj,tk,x1,x2,x3,r,h,ph,gdet = gd[:,:,:,:].view() 
+    #get the right order of indices by reversing the order of indices i,j(,k)
+    #conn=gd[9:73].view().reshape((4,4,4,nx,ny,nz), order='F').transpose(2,1,0,3,4,5)
+    #gn3 = gd[73:89].view().reshape((4,4,nx,ny,nz), order='F').transpose(1,0,2,3,4)
+    #gv3 = gd[89:105].view().reshape((4,4,nx,ny,nz), order='F').transpose(1,0,2,3,4)
+    #gdet = gd[105]
+    #ck = gd[106:110].view().reshape((4,nx,ny,nz), order='F')
+    #dxdxp = gd[110:136].view().reshape((4,4,nx,ny,nz), order='F').transpose(1,0,2,3,4)
+
+def rfdgrid(dumpname): #read gdump: header and body
+    global nx,ny,nz,_dx1,_dx2,_dx3,ti,tj,tk,x1,x2,x3,r,h,ph,conn,gn3,gv3,ck,dxdxp,gdet
+    print( "Reading grid from " + "dumps/" + dumpname + " ..." )
+    gin = open( "dumps/" + dumpname, "rb" )
+    header = gin.readline().split()
+    nx = int(header[1])
+    ny = int(header[2])
+    nz = int(header[3])
+    _dx1=float(header[7])
+    _dx2=float(header[8])
+    _dx3=float(header[9])
+    gin.close()
+    #read gdump
+    #
+    gd = np.loadtxt( "dumps/" + dumpname, 
+                      dtype=float, 
+                      skiprows=1, 
+                      unpack = True,
+                      usecols=(0,1,2,3,4,5,6,7,8)).view().reshape((-1,nx,ny,nz), order='F')
+    #gd = np.genfromtxt( "dumps/gdump", 
+    #                 dtype=float, 
+    #                 skip_header=1, 
+    #                 skip_footer=nx*ny*(nz-1),
+    #                 unpack = True ).view().reshape((137,nx,ny,nz), order='F')
+    ti,tj,tk,x1,x2,x3,r,h,ph,gdet = gd[:,:,:,:].view() 
 
 def mdot(a,b):
     """
@@ -199,9 +419,28 @@ def fieldcalc():
     """
     Computes the field vector potential
     """
-    daphi = (gdet*B[1]).sum(2)*_dx2*_dx3
+    #return((1-h[:,:,0]/np.pi)[:,:,None]*fieldcalcp()+(h[:,:,0]/np.pi)[:,:,None]*fieldcalcm())
+    return(fieldcalcU())
+
+def mysum2(vec):
+    #return( vec[:,:,0][:,:,None]*nz )
+    return( np.sum(vec, axis=2)[:,:,None] )
+
+def fieldcalcp():
+    """
+    Computes the field vector potential
+    """
+    daphi = mysum2(gdet*B[1])*_dx2*_dx3
     aphi=daphi.cumsum(axis=1)
-    return(aphi[:,:,None])
+    return(aphi)
+
+def fieldcalcm():
+    """
+    Computes the field vector potential
+    """
+    daphi = mysum2(gdet*B[1])*_dx2*_dx3
+    aphi=(-daphi[:,::-1].cumsum(axis=1))[:,::-1]
+    return(aphi)
 
 def fieldcalc2():
     """
@@ -212,6 +451,19 @@ def fieldcalc2():
     daphi = (-gdet*B[2]).sum(2)*_dx2*_dx3
     #sc.zeros_like(B[1,:,:,0:1])
     daphi1 = (gdet[0]*B[1,0]).sum(1).cumsum(axis=0)*_dx2*_dx3
+    daphi[0,:] += daphi1
+    aphi=daphi.cumsum(axis=0)
+    return(aphi[:,:,None])
+
+def fieldcalc2U():
+    """
+    Computes the field vector potential
+    """
+    #daphi = (gdet*B[1]).sum(2)*_dx2*_dx3
+    #aphi=daphi.cumsum(axis=1)
+    daphi = (-gdetB[2]).sum(2)*_dx2*_dx3
+    #sc.zeros_like(B[1,:,:,0:1])
+    daphi1 = (gdetB[1,0]).sum(1).cumsum(axis=0)*_dx2*_dx3
     daphi[0,:] += daphi1
     aphi=daphi.cumsum(axis=0)
     return(aphi[:,:,None])
@@ -253,7 +505,7 @@ def fhorvstime(ihor):
     mdot=np.empty(len(flist),dtype=float)
     for findex, fname in enumerate(flist):
         print( "Reading " + fname + " ..." )
-        jrdp3d("../"+fname)
+        rfd("../"+fname)
         fs[findex]=horfluxcalc(ihor)
         mdot[findex]=mdotcalc(ihor)
         ts[findex]=t
@@ -264,6 +516,7 @@ def plotit(ts,fs,md):
     #rc('font', family='serif')
     #plt.figure( figsize=(12,9) )
     fig,plotlist=plt.subplots(nrows=2,ncols=1,sharex=True,figsize=(12,9))
+    #plt.subplots_adjust(hspace=0.4) #increase vertical spacing to avoid crowding
     plotlist[0].plot(ts,fs,label=r'$\Phi_{\rm h}/0.5\Phi_{\rm i}$: Normalized Horizon Magnetic Flux')
     plotlist[0].plot(ts,fs,'r+', label=r'$\Phi_{\rm h}/0.5\Phi_{\rm i}$: Data Points')
     plotlist[0].legend(loc='lower right')
@@ -278,7 +531,7 @@ def plotit(ts,fs,md):
     plotlist[1].legend(loc='upper right')
     plotlist[1].set_xlabel(r'$t\;(GM/c^3)$')
     plotlist[1].set_ylabel(r'$\dot M_{\rm h}$',fontsize=16)
-
+    
     #title("\TeX\ is Number $\displaystyle\sum_{n=1}^\infty\frac{-e^{i\pi}}{2^n}$!", 
     #      fontsize=16, color='r')
     plotlist[1].grid(True)
@@ -298,21 +551,45 @@ def test():
 if __name__ == "__main__":
     import sys
     #mainfunc()
-    grid3d("gdump")
-    jrdp3d("fieldline0250.bin")
+    #grid3d("gdump")
+    #rfd("fieldline0250.bin")
     #cvel()
     #plc(rho)
-    #jrdp3d("fieldline0000.bin")
+    #rfd("fieldline0000.bin")
     #diskflux=diskfluxcalc(ny/2)
     #ts,fs,md=fhorvstime(10)
     #plotit(ts,fs/(0.5*diskflux),md)
-    #jrdp3d("fieldline0320.bin")
-    plt.figure(1)
-    aphi=fieldcalc()
-    plc(aphi)
-    plt.figure(2)
-    aphi2=fieldcalc2()
-    plc(aphi2)
+    #rfd("fieldline0320.bin")
+    #plt.figure(1)
+    #aphi=fieldcalc()
+    #plc(aphi)
+    #plt.figure(2)
+    #aphi2=fieldcalc2()
+    #plc(aphi2)
     #test()
-
-
+    if False:
+        flist = glob.glob( os.path.join("dumps/", "fieldline*.bin") )
+        for findex, fname in enumerate(flist):
+            print( "Reading " + fname + " ..." )
+            rfd("../"+fname)
+            mkframe("lrho%04d" % findex, vmin=-8,vmax=0.2)
+            plt.clf()
+        print( "Done!" )
+    if False:
+        #rfd("fieldline0000.bin")  #to define _dx#
+        #grid3dlight("gdump")
+        flist = glob.glob( os.path.join("dumps/", "rdump--*") )
+        for findex, fname in enumerate(flist):
+            print( "Reading " + fname + " ..." )
+            rrdump("../"+fname)
+            plt.clf()
+            mkframe("lrho%04d" % findex, vmin=-8,vmax=0.2)
+        print( "Done!" )
+    #
+    #plt.clf(); rfd("fieldline0000.bin"); aphi=fieldcalc(); plc(ug/bsq) 
+    #rfd("fieldline0002.bin")
+    if True:
+        grid3d("gdump"); rfd("fieldline0000.bin"); rrdump("rdump--0000"); plt.clf(); cvel(); plc(bsq,cb=True)
+        plt.clf();plt.plot(x1[:,ny/2,0],(bsq/(2*(gam-1)*ug))[:,ny/2,0])
+        plt.plot(x1[:,ny/2,0],(bsq/(2*(gam-1)*ug))[:,ny/2,0],'+')
+        plt.plot(x1[:,ny/2,0],(0.01*rho)[:,ny/2,0])
