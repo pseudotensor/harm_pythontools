a908348d948939daa89d61c97efb3e0b22cd16d2
Alexander Tchekhovskoy <atchekho@princeton.edu>
2011-03-31 23:56:28 -0400

    Still need fieldline0000.bin, to know precision of rho for definition of tiny

--- mread/__init__.py
+++ mread/__init__.py
@@ -2175,7 +2175,7 @@ if __name__ == "__main__":
         #cd ~/run; for f in rtf*; do cd ~/run/$f; (nice -n 10 python  ~/py/mread/__init__.py &> python.out); done
         grid3d("gdump.bin")
         #rd("dump0000.bin")
-        #rfd("fieldline0000.bin")
+        rfd("fieldline0000.bin")
         rhor=1+(1-a**2)**0.5
         ihor = np.floor(iofr(rhor)+0.5);
         #diskflux=diskfluxcalc(ny/2)
