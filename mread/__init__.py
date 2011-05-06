cb44ce15c10077ae7c1117195915dcb2645c9203
Alexander Tchekhovskoy <atchekho@princeton.edu>
2011-05-06 01:27:16 -0400

    mask the BH in xy plane -- may help avoid artifacts

--- mread/__init__.py
+++ mread/__init__.py
@@ -1002,8 +1002,8 @@ def mkframexy(fname,ax=None,cb=True,vmin=None,vmax=None,len=20,ncell=800,pt=True
         Bxnorm=BRnorm*np.cos(ph)-Bpnorm*np.sin(ph)
         Bynorm=BRnorm*np.sin(ph)+Bpnorm*np.cos(ph)
         #
-        iBx = reinterpxy(Bxnorm,extent,ncell,domask=0)
-        iBy = reinterpxy(Bynorm,extent,ncell,domask=0)
+        iBx = reinterpxy(Bxnorm,extent,ncell,domask=0.4)
+        iBy = reinterpxy(Bynorm,extent,ncell,domask=0.4)
         iibeta = reinterpxy(0.5*bsq/(gam-1)/ug,extent,ncell,domask=0)
         ibsqorho = reinterpxy(bsq/rho,extent,ncell,domask=0)
         ibsqo2rho = 0.5 * ibsqorho
