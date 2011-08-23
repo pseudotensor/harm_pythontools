89795e2c6ebf31eba36aae6797bf9ace63afd872
Alexander Tchekhovskoy <atchekho@princeton.edu>
2011-08-23 13:21:58 -0400

    final correction to pjke

--- mread/__init__.py
+++ mread/__init__.py
@@ -4531,9 +4531,9 @@ def takeoutfloors(ax=None,doreload=1,dotakeoutfloors=1,dofeavg=0,fti=None,ftf=No
         rj = 100
         foutpower.write( "%s %f %f %f %f %f %f %f %f\n" % (os.path.basename(os.getcwd()), a, 
                                                            eta[iofr(rx)], spar[iofr(rx)], 
-                                                           Fm[iofr(rx)], Fe[iofr(rx)], Fl[iofr(rx)]/dxdxp[3][3][:,0,0],
-                                                           float(pjke_mu2_avg[iofr(rj)]), 
-                                                           float((pjke_mu1_avg-pjke_mu2_avg)[iofr(rj)]) ) )
+                                                           Fm[iofr(rx)], Fe[iofr(rx)], Fl[iofr(rx)]/dxdxp[3][3][0,0,0],
+                                                           pjke_mu2_avg[iofr(rj)], 
+                                                           (pjke_mu1_avg-pjke_mu2_avg)[iofr(rj)] ) )
         #flush to disk just in case to make sure all is written
         foutpower.flush()
         os.fsync(foutpower.fileno())
