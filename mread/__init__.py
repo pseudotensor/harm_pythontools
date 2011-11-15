c42e18cd8b2612af2e6b635f748cdb7fd5302eb7
Alexander Tchekhovskoy <atchekho@princeton.edu>
2011-11-15 11:34:17 -0500

    Add section for angular momentum flow

--- mread/__init__.py
+++ mread/__init__.py
@@ -6828,6 +6828,97 @@ def mkstreamlinefigure(length=25,doenergy=False,frac=0.75,frameon=True,dpi=300,s
         plt.xlim(-mylenshow,mylenshow)
         plt.ylim(-mylenshow,mylenshow)
         #pdb.set_trace()
+    if False:
+        #Angular momentum flow plot
+        if not avg_gdetF[0,0].any():
+            #saved face-centered fluxes exist
+            is_output_cell_center = True
+            enden1=(-gdet*avg_Tud[1,0]-gdet*avg_rhouu[1])*nz
+            enden2=(-gdet*avg_Tud[2,0]-gdet*avg_rhouu[2])*nz
+            enden=enden1
+            mdden=(-gdet*avg_rhouu[1])*nz
+        else:
+            is_output_cell_center = False
+            #x1-fluxes of:
+            #0,0 mass   
+            #0,1 energy 
+            #0,2 ang.m. 
+            #x2-fluxes of:
+            #0,0 mass   
+            #0,1 energy 
+            #0,2 ang.m. 
+            enden1=(-avg_gdetF[0,1]*nz)
+            enden2=(-avg_gdetF[1,1]*nz)
+            enden=enden1
+            mdden =(-avg_gdetF[0,0]*nz)
+        if dotakeoutfloors:
+            DFfloor=takeoutfloors(ax=None,doreload=1,dotakeoutfloors=dotakeoutfloors,dofeavg=0,isinteractive=0,writefile=False,doplot=False,aphi_j_val=0, ndim=2, is_output_cell_center = False)
+            #subtract rest-mass from total energy flux and flip the sign to get correct direction
+            DFen = DFfloor[1]+DFfloor[0]
+            #pdb.set_trace()
+            enden += DFen[:,:,None]/(_dx2*_dx3)
+            mdden += DFfloor[0][:,:,None]/(_dx2*_dx3)
+        en=(enden.cumsum(1)-0.5*enden)*_dx2*_dx3 #subtract half of current cell's density to get cell-centered quantity
+        md=(mdden).sum(2).sum(1)*_dx2*_dx3
+        #pdb.set_trace()
+        if is_output_cell_center == False:
+            en[:-1]=0.5*(en[:-1]+en[1:])
+            enden1[:-1]=0.5*(enden1[1:]+enden1[:-1])
+            enden2[:,:-1]=0.5*(enden2[:,1:]+enden2[:,:-1])
+            md[:-1]=0.5*(md[:-1]+md[1:])
+        #mdot vs. radius
+        #pick out a scalar value at r = 5M
+        md=md[iofr(5)]
+        #equatorial trajectory: starts at r = rh, theta = pi/2
+        rhor=1+(1-a**2)**0.5
+        radval=10.
+        if is_output_cell_center == True:
+            #internal fluxes not available
+            traj = mkonestreamlinex1x2( -avg_Tud[1,0,:,:,0]-avg_rhouu[1,:,:,0],
+                                    -avg_Tud[2,0,:,:,0]-avg_rhouu[2,:,:,0],
+                                    x1[:,0,0],x2[0,:,0],
+                                    x1[iofr(radval),ny/2,0],0.)
+        else:
+            #internal fluxes available: use them (account for the fact that they are staggered!)
+            traj = mkonestreamlinex1x2( enden1[:,:,0],
+                                    enden2[:,:,0],
+                                    x1[:,0,0],x2[0,:,0],
+                                    x1[iofr(radval),ny/2,0],0.)
+        xtraj,ytraj=traj
+        x1traj=x1[:,0,0]
+        x2traj=interp1d(xtraj, ytraj, kind='linear',bounds_error=False)(x1traj)
+        entraj=findroot2d(x2[:,:,0]-x2traj[:,None], en[:,:,0], axis = 0, isleft = True )
+        #change zero to be at the equatorial field line
+        en=en-entraj[:,None,None]
+        r2=np.concatenate((r[:,::-1],r),axis=1)
+        h2=np.concatenate((-h[:,::-1],h),axis=1)
+        en2=np.concatenate((en[:,::-1],en),axis=1)
+        #adjust the last cell positions to ensure last contour is closed
+        h2[:,0]=h2[:,0]*0-np.pi*1.
+        h2[:,-1]=h2[:,-1]*0+np.pi*1.
+        #plc(en2,xcoord=r2*np.sin(h2),ycoord=r2*np.cos(h2),cb=True,nc=20,isfilled=True)
+        z = np.abs(en2)/np.nanmax(np.abs(en2))
+        minval=1e-3
+        cutval=1*minval
+        z[z<cutval]=z[z<cutval]*0+cutval
+        # lev_exp = np.arange(np.floor(np.log10(np.nanmin(z))-1),
+        #                      np.ceil(np.log10(np.nanmax(z))+1))
+        lev_exp=np.linspace(np.log10(minval),0,10)
+        levs = np.power(10, lev_exp)
+        #plco(z,xcoord=r2*np.sin(h2),ycoord=r2*np.cos(h2),cb=True,nc=20,levels=levs,isfilled=True,norm=colors.LogNorm())
+        ctsf=plc(z,xcoord=r2*np.sin(h2),ycoord=r2*np.cos(h2),cb=False,nc=20,levels=levs,isfilled=True,locator=ticker.LogLocator(),alpha=0.25,zorder=2,cmap=cm.hot_r)
+        cts=plc(z,xcoord=r2*np.sin(h2),ycoord=r2*np.cos(h2),cb=False,nc=20,levels=ctsf.levels[1::1],isfilled=False,locator=ticker.LogLocator(),alpha=0.25,zorder=2,linestyles='solid',linewidths=0.5,colors='r')
+        # Make a colorbar for the ContourSet returned by the contourf call.
+        cbar = plt.colorbar(ctsf)
+        #cbar.ax.set_ylabel('verbosity coefficient')
+        # Add the contour line levels to the colorbar
+        cbar.add_lines(cts)
+        #pdb.set_trace()
+        #plt.xlim(-30,30); plt.ylim(-30,30)
+        mylenshow = frac*mylen
+        plt.xlim(-mylenshow,mylenshow)
+        plt.ylim(-mylenshow,mylenshow)
+        #pdb.set_trace()
     if True:
         istag, jstag, hstag, rstag = getstagparams(doplot=0,usedefault=usedefault)
         myRmax=4
