Fix to avoid using gdetF when only partial data available over the averaging interval

mread/__init__.py
@@ -3620,7 +3620,8 @@ def plotqtyvstime(qtymem,ihor=None,whichplot=None,ax=None,findex=None,fti=None,f
 
     #mdotiniavg = timeavg(mdtot[:,ihor]-md10[:,ihor],ts,fti,ftf)
     #mdotfinavg = (mdtot[:,ihor]-md10[:,ihor])[(ts<ftf)*(ts>=fti)].sum()/(mdtot[:,ihor]-md10[:,ihor])[(ts<ftf)*(ts>=fti)].shape[0]
-    if True and (gdetF11!=0).any():
+    if True and (gdetF11!=0).any() and timeavg(gdetF11,ts,fti,fti+100.).any():
+        print( "plotqtyvstime(): using gdetF11 and gdetF10 to compute edot and mdot" )
         edtot[:-1] = -0.5*(gdetF11[:-1]+gdetF11[1:])
         mdtot[:-1] = -0.5*(gdetF10[:-1]+gdetF10[1:])
         edtot -= mdtot
@@ -5417,11 +5418,12 @@ def takeoutfloors(ax=None,doreload=1,dotakeoutfloors=1,dofeavg=0,fti=None,ftf=No
                 pjke_mu2_avg, pjke_mu1_avg, \
                 gdetF10, gdetF11, gdetF12 \
                 = plotqtyvstime( qtymem, whichplot = -200, fti=fti, ftf=ftf, aphi_j_val=aphi_j_val )
-
+    
     if np.abs(a - 0.99)<1e-4 and scaletofullwedge(1.0) < 1.5:
         #correct energy flux for face vs. center
         edtotvsr = (edtotvsr+mdtotvsr)*energy_flux_correction_factor - mdtotvsr
-    elif True and (gdetF11!=0).any():
+    elif True and (gdetF11!=0).any() and timeavg(qtymem[132],qtymem[0,:,0],fti,fti+100.).any():
+        print( "takeoutfloors(): using gdetF11 and gdetF10 to compute edot and mdot" )
         edtotvsr[:-1] = -0.5*(gdetF11[:-1]+gdetF11[1:])
         mdtotvsr[:-1] = -0.5*(gdetF10[:-1]+gdetF10[1:])
         edtotvsr -= mdtotvsr