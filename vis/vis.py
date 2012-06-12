aa0f3a9cfa17d9cc1945b95a069c517a5330a8f0
Alexander Tchekhovskoy <atchekho@princeton.edu>
2012-06-12 17:00:37 -0400

    initial import

---
+++ vis/vis.py
@@ -0,0 +1,13 @@
+import visit_writer
+import numpy as np
+
+def genmesh():
+    x = r*np.sin(h)*cos(ph)
+    y = r*np.sin(h)*sin(ph)
+    z = r*np.cos(h)
+    pts = np.array(zip(x,y,z)).reshape(-1)
+    
+
+
+if __name__ == "__main__":
+    return
