e47a460ed0cd7a6a18f43bd351b1472717bd6dc7
Sasha <atchekho@princeton.edu>
2012-06-18 00:17:06 +0100

    dipole coord transforms

--- vis/visit_script.py
+++ vis/visit_script.py
@@ -72,10 +72,11 @@ def get_visit_time()
 
 def rotate_around_y(r, th, ph, alpha=0):
     """Returns x, y, z of the rotated vector"""
+    alpha = -alpha
     xp = r*(-np.cos(th)*np.sin(alpha)+np.sin(th)*np.cos(ph)*np.cos(alpha))
     yp = r*np.sin(th)*np.cos(ph)
     zp = r*(np.cos(th)*np.cos(alpha)+np.sin(th)*np.cos(ph)*np.sin(alpha))
-    return xp, yz, zp
+    return xp, yp, zp
 
 def rotate_around_z(x, y, z, alpha=0):
     x = xp * np.cos(alpha) - yp * np.sin(alpha)
@@ -85,8 +86,21 @@ def rotate_around_z(x, y, z, alpha=0):
 
 def rotate_around_y_z(r, th, ph, alpha_y=0, alpha_z=0):
     xp, yp, zp = rotate_around_y(r,th,ph,alpha=alpha_y)
-    x, y, z = rotate_around_y(x,y,z,alpha=alpha_z)
-    return x, y, z    
+    x, y, z = rotate_around_y(xp,yp,zp,alpha=alpha_z)
+    return x, y, z 
+
+def compute_footpoints(r0 = 1.5, Rlc=5,npts=10,whichpole="up", alpha_y=0, alpha_z=0):
+    thp = (r0/Rlc)**0.5
+    ph = 2*np.pi*np.arange(0,1,1./npts)
+    r = r0 + np.zeros_like(ph)
+    th = thp + np.zeros_like(ph)
+    return rotate_around_y_z(r, th, ph,alpha_y=alpha_y, alpha_z=alpha_z)
+
+def zip2visit(xyz):
+    return np.ravel(zip(*xyz))
+
+def get_visit_footpoints(r0 = 1.5, Rlc=5, npts=10, whichpole="up", alpha_y=0, alpha_z=0):
+    zip2visit(compute_footpoints(r0=r0, Rlc=Rlc, npys=npts, whichpole=whichpole, alpha_y=alpha_y, alpha_z=alpha_z))
 
 
 if __name__ == "__main__":
