95590218e695ffdf56730af6dfaef6614af991b8
Alexander Tchekhovskoy <atchekho@princeton.edu>
2013-02-05 19:50:47 -0500

    upd

--- mread/__init__.py
+++ mread/__init__.py
@@ -1344,20 +1344,22 @@ def writemanyvarstotxt(fname="file"):
 
 #for Sasha Philippov and Jason Li
 def converttotxt():
-    runlist = [ #"hf_0_h10r05_om02_ffde_mydt_sph_ps0_2048x1024_64x64x1",
-                "hf_15_r10h05_mydt_sph_ps2_256x128x128_bsqorho",
-                "hf_30_r10h05_mydt_sph_x2_bsqorho50",
-                "hf_45_r10h05_mydt_sph_ps2_256x128x128_32x16x32_bsqorho50",
-                "hf_60_r10h05_mydt_sph_ps2_256x128x128_512_bsqorho50",
-                "hf_75_r10h05_mydt_sph_ps2_256x128x128_256_bsqorho50",
-                "hf_90_r10h05_mydt_sph_x2_bsqorho50" ]
-    n1n2 = [ #[400,401],
-			 [140,141],
-			 [140,141],
-			 [140,141],
-			 [106,107],
-			 [93,94],
-			 [159,160] ]
+    runlist = [ "hf_0_h10r05_om02_ffde_mydt_sph_ps0_2048x1024_64x64x1",
+                # "hf_15_r10h05_mydt_sph_ps2_256x128x128_bsqorho",
+                # "hf_30_r10h05_mydt_sph_x2_bsqorho50",
+                # "hf_45_r10h05_mydt_sph_ps2_256x128x128_32x16x32_bsqorho50",
+                # "hf_60_r10h05_mydt_sph_ps2_256x128x128_512_bsqorho50",
+                # "hf_75_r10h05_mydt_sph_ps2_256x128x128_256_bsqorho50",
+                # "hf_90_r10h05_mydt_sph_x2_bsqorho50"
+				]
+    n1n2 = [ [400,401],
+			 # [140,141],
+			 # [140,141],
+			 # [140,141],
+			 # [106,107],
+			 # [93,94],
+			 # [159,160]
+			 ]
     for (i,f) in enumerate(runlist):
         os.chdir("/home/atchekho/run2/%s" % f)
         loadandwritevars(n1=n1n2[i][0],n2=n1n2[i][1])
