b2b4f00ca6c7f7975b0c30ef6ac60e31099c5f7e
Jonathan McKinney <jcm@umd.edu>
2013-07-12 15:20:24 -0400

    minor fixed for avg plotting and small number of files.

--- scripts/makemovie.sh
+++ scripts/makemovie.sh
@@ -1590,8 +1590,25 @@ echo $passpart1$passpart2 | /usr/kerberos/bin/kinit
 #
 ####################################
 
+numfiles=`find dumps/ -name "fieldline*.bin"|wc -l`
+
+echo "NUMFILES=$numfiles"
+
 #itemspergroup=$(( 1 )) # MAVARA
 itemspergroup=$(( 20 ))
+
+# catch too small number of files
+# must match __init__.py
+if [ $numfiles -le $itemspergroup ]
+then
+    if [ $numfiles -eq 1 ]
+    then
+        itemspergroup=1
+    else
+        itemspergroup=$(( $numfiles - 1))
+    fi
+fi
+
 itemspergrouptext=`printf "%02d"  "$itemspergroup"`
 
 
