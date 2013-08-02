cd4a05ad961005faef98ea21dd1b77d1f5541c05
Alexander Tchekhovskoy <atchekho@princeton.edu>
2013-08-02 15:07:50 -0400

    added Komi's version of the stag surface

--- mread/__init__.py
+++ mread/__init__.py
@@ -16,6 +16,7 @@ matplotlib.rcParams['text.latex.preamble']=[r"\usepackage{amssymb,amsmath}"]
 
 #from pylab import figure, axes, plot, xlabel, ylabel, title, grid, savefig, show
 
+import time as time
 import gc
 import numpy as np
 import array
@@ -45,6 +46,40 @@ import visit_writer
 
 #global rho, ug, vu, uu, B, CS
 #global nx,ny,nz,_dx1,_dx2,_dx3,ti,tj,tk,x1,x2,x3,r,h,ph,gdet,conn,gn3,gv3,ck,dxdxp
+
+#so far only for hydro
+def rdath(fname):
+    global n1, n2, t, ti, tj, x1, x2, rho, v1, v2, v3, pg
+    fin = open( fname , "rb" )
+    header1 = fin.readline().split()
+    header2 = fin.readline().split()
+    header3 = fin.readline().split()
+    header4 = fin.readline().split()
+    header5 = fin.readline().split()
+    n1 = np.float64(header1[3].split("'")[0])
+    n2 = np.float64(header3[3].split("'")[0])
+    t = np.float64(header5[6].split(",")[0])
+    res = np.loadtxt(fname,dtype=np.float64,skiprows=0,unpack=1).reshape((-1,n1,n2),order="F")
+    ti, tj, x1, x2, rho, v1, v2, v3, pg = res
+
+def mkathtestmovie(sleepdt=0.5,**kwargs):
+    fntsize = kwargs.pop("fontsize",20)
+    plt.clf()
+    for i in xrange(0,21):
+        rdath("jetblob.%04d.tab"%i);
+        #plco(np.log10(pg),levels=np.arange(-4,4,0.1),isfilled=1,antialiased=0,cb=1);
+        ax = plt.gca()
+        # plt.clf()
+        p=ax.imshow(np.log10(pg[:,:].transpose()), extent=(0,n1,0,n2), cmap = cm.jet, norm = colors.Normalize(clip = True),origin='lower',interpolation="nearest",vmin=-4,vmax=4,**kwargs)
+        if i==0:
+            cbar = plt.colorbar(p)
+            cbar.ax.set_ylabel(r'$\log_{10}p$',fontsize=fntsize)
+
+        plt.title(r"$t=%g$" % t)
+        plt.draw();
+        time.sleep(sleepdt)
+        plt.savefig("frame%04d.png"%i,bbox_inches='tight',pad_inches=0.02)
+
 
 def plot_current():
     #works on jemmy
@@ -212,6 +247,15 @@ def omegaf_plot(fntsize=20,useavgs=0):
     ax = plt.gca()
     for label in ax.get_xticklabels() + ax.get_yticklabels():
         label.set_fontsize(fntsize)
+
+def stag_compute_analytic(**kwargs):
+    Delta = r**2+a**2-2*r
+    Sigma = (r**2+a**2)**2-a**2*Delta*np.sin(h)**2
+    OmegaFIDO = 2*a*r/Sigma
+    rhor = 1+(1-a**2)**0.5
+    OmegaH = a/(2*rhor)
+    return(OmegaH/2.-OmegaFIDO)
+    #plc(OmegaH/2.-OmegaFIDO,levels=(0,),xcoord=r*sin(h),ycoord=r*cos(h),**kwargs)
 
 def eminusomegal_plot(fntsize=20,useavgs=0,doreload=0,fname=None,dofig=False):
     global B
@@ -433,6 +477,10 @@ def eminusomegal_plot(fntsize=20,useavgs=0,doreload=0,fname=None,dofig=False):
         ax.plot(xs2,zs2,'b-',lw=linewidth,zorder=21)
     else:
         plc(uu2,xcoord=r2*np.sin(h2),ycoord=r2*np.cos(h2),levels=(0,),colors="b",linewidths=2)
+    res = stag_compute_analytic()
+    res1 = np.concatenate((res[:,::-1],res),axis=1)
+    res2 = np.concatenate((res1[:,::-1],res1),axis=1)
+    plc(res2,xcoord=r2*np.sin(h2),ycoord=r2*np.cos(h2),levels=(0,),colors="Orange",linewidths=2)
     #
     if dofig:
         if useavgs:
