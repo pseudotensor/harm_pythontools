b1f4ed2ca064a1d5bf097dc0c1d2789e3819274e
Alexander Tchekhovskoy <atchekho@princeton.edu>
2013-09-14 23:17:28 -0700

    Modify athena macros for 3d and movie making

--- mread/__init__.py
+++ mread/__init__.py
@@ -48,8 +48,8 @@ import visit_writer
 #global nx,ny,nz,_dx1,_dx2,_dx3,ti,tj,tk,x1,x2,x3,r,h,ph,gdet,conn,gn3,gv3,ck,dxdxp
 
 #so far only for hydro
-def rdath(fname):
-    global n1, n2, t, ti, tj, x1, x2, rho, v1, v2, v3, pg
+def rdath2d(fname):
+    global n1, n2, n3, t, ti, tj, tk, x1, x2, x3, rho, v1, v2, v3, pg
     fin = open( fname , "rb" )
     header1 = fin.readline().split()
     header2 = fin.readline().split()
@@ -62,26 +62,47 @@ def rdath(fname):
     res = np.loadtxt(fname,dtype=np.float64,skiprows=0,unpack=1).reshape((-1,n1,n2),order="F")
     ti, tj, x1, x2, rho, v1, v2, v3, pg = res
 
+def rdath3d(fname):
+    global n1, n2, n3, t, ti, tj, tk, x1, x2, x3, rho, v1, v2, v3, pg, B1c, B2c, B3c
+    fin = open( fname , "rb" )
+    header1 = fin.readline().split()
+    header2 = fin.readline().split()
+    header3 = fin.readline().split()
+    header4 = fin.readline().split()
+    header5 = fin.readline().split()
+    header6 = fin.readline().split()
+    header7 = fin.readline().split()
+    n1 = np.float64(header1[3].split("'")[0])
+    n2 = np.float64(header3[3].split("'")[0])
+    n3 = np.float64(header5[3].split("'")[0])
+    t = np.float64(header7[6].split(",")[0])
+    fin.close()
+    res = np.loadtxt(fname,dtype=np.float64,skiprows=0,unpack=1).reshape((-1,n1,n2,n3),order="F")
+    ti, tj, tk, x1, x2, x3, rho, v1, v2, v3, pg, B1c, B2c, B3c = res
+
 def mkathtestmovie(**kwargs):
     fntsize = kwargs.pop("fontsize",20)
-    name=kwargs.pop("name","jetblob")
+    name=kwargs.pop("name","str-flux-cap")
     sleepdt=kwargs.pop("sleepdt",0.5)
-    ndump=kwargs.pop("ndump",30)
+    startn=kwargs.pop("startn",0)
+    endn=kwargs.pop("endn",112)
+    dn=kwargs.pop("dn",1)
+    dosavefig=kwargs.pop("dosavefig",1)
     plt.clf()
-    for i in xrange(0,21):
-        rdath("%s.%04d.tab"%(name,i));
+    for i in xrange(startn,endn,dn):
+        rdath3d("%s.%04d.tab"%(name,i));
         #plco(np.log10(pg),levels=np.arange(-4,4,0.1),isfilled=1,antialiased=0,cb=1);
         ax = plt.gca()
-        # plt.clf()
-        p=ax.imshow(np.log10(pg[:,:].transpose()), extent=(0,n1,0,n2), cmap = cm.jet, norm = colors.Normalize(clip = True),origin='lower',interpolation="nearest",vmin=-4,vmax=4,**kwargs)
-        if i==0:
+        #plt.clf()
+        p=ax.imshow(np.log10(pg[:,:,n3/2].transpose()), extent=(0,n1,0,n2), cmap = cm.jet, norm = colors.Normalize(clip = True),origin='lower',interpolation="nearest",vmin=-3,vmax=3,**kwargs)
+        if i==startn:
             cbar = plt.colorbar(p)
             cbar.ax.set_ylabel(r'$\log_{10}p$',fontsize=fntsize)
-
         plt.title(r"$t=%g$" % t)
         plt.draw();
         time.sleep(sleepdt)
-        plt.savefig("frame%04d.png"%i,bbox_inches='tight',pad_inches=0.02)
+        if dosavefig:
+            plt.savefig("frame%04d.png"%i,bbox_inches='tight',pad_inches=0.02)
 
 
 def plot_current():
