4dbece3b545a8ee96be35655e74ce1785a0a6c05
Alexander Tchekhovskoy <atchekho@berkeley.edu>
2014-02-01 13:22:26 -0800

    arrangements

--- mread/pulsar_br.py
+++ mread/pulsar_br.py
@@ -225,19 +225,25 @@ def plotbrsq(cachefname="psrangle.npz",alpha = 15,fntsize=20,dosavefig=0):
     #
     # Mlab Fig. 1
     #
-    scene = mlab.figure(1, bgcolor=(1, 1, 1), fgcolor=(0, 0, 0), size=(800, 600))
+    scene = mlab.figure(1, bgcolor=(1, 1, 1), fgcolor=(0, 0, 0), size=(900, 600))
     #engine = mlab.get_engine()
     mlab.clf()
     i = 0
     A = 1.1
-    al_list = [15, 30, 60, 90]
+    al_list = [0, 15, 30, 60, 90]
     l = len(al_list)
     for al in al_list:
         Br_sm = v["Br2d%g"%al]/(v["psi%g" % al]/v["psi0"])/norm
         Br_ft = Br_fit(al)
         th = v["th2d%g"%al]
         ph = v["ph2d%g"%al]
+        if al == 0:
+            ph = np.linspace(0,2*np.pi,128,endpoint=0)[None,:] + 0*th
+            th = th + 0*ph
+            Br_sm = Br_sm + 0*th
+            Br_ft = Br_ft + 0*th
         r = 1
+        # pdb.set_trace()
         s_sim = wraparound(np.abs(Br_sm))
         s_fit = wraparound(np.abs(Br_ft))
         x = wraparound(r*sin(th)*cos(ph))
@@ -247,23 +253,14 @@ def plotbrsq(cachefname="psrangle.npz",alpha = 15,fntsize=20,dosavefig=0):
         mlab.mesh(x+A*3*(i-0.5*l+0.5), y, z+1.5*A, scalars=s_fit, colormap='jet',vmin=np.min(s_fit), vmax = np.max(s_fit))
         i = i + 1
         #pdb.set_trace()
-    # scene.scene.camera.position = [0, -17, 0]
-    # scene.scene.camera.focal_point = [0.0, 0.0, 0.0]
-    # scene.scene.camera.view_angle = 30.0
-    # scene.scene.camera.view_up = [0, 0, 1]
-    # scene.scene.camera.clipping_range = [22.635607220310334, 29.075029212650286]
-    # scene.scene.camera.compute_view_plane_normal()
-    # 
-    # scene.scene.render()
-    scene.scene.camera.position = [-0.0, -17, 0.0]
+    scene.scene.parallel_projection = True
+    scene.scene.camera.position = [0.0, -19.834710743801651, 0.0]
     scene.scene.camera.focal_point = [0.0, 0.0, 0.0]
     scene.scene.camera.view_angle = 30.0
     scene.scene.camera.view_up = [0.0, 0.0, 1.0]
-    # scene.scene.camera.clipping_range = [14.634420004931805, 20.018782858113472]
-    # scene.scene.camera.compute_view_plane_normal()
+    scene.scene.camera.clipping_range = [17.646366008628497, 22.637228418765972]
+    scene.scene.camera.zoom(1.6)
     scene.scene.show_axes = True
-    scene.scene.render()
-    #mlab.view(focalpoint=[0,0,0],distance=25)
     v.close()
     
 def wraparound(v):
