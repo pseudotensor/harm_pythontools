c9b299bbaf3060c3955d0b3c4103b49cdb926750
Jonathan McKinney <pseudotensor@gmail.com>
2016-08-08 04:30:01 -0400

    avoid excess file creation2

--- scripts/makeallmovie.sh
+++ scripts/makeallmovie.sh
@@ -39,7 +39,7 @@ dircollecttilt="thickdiskfull3d7tilt0.35 thickdiskfull3d7tilt0.7 thickdiskfull3d
 #################
 # choose:
 #dircollect="rad1"
-dircollect="jonharmrad1 jonharmrad2 jonharmrad3 jonharmrad5 jonharmrad7 jonharmrad8 jonharmrad9 jonharmrad10 jonharmrad11 jonharmrad13 jonharmrad14 jonharmrad15 jonharmrad16 jonharmrad17"
+dircollect="jonharmrad1 jonharmrad2 jonharmrad3 jonharmrad5 jonharmrad7 jonharmrad8 jonharmrad9 jonharmrad10 jonharmrad11 jonharmrad13 jonharmrad14 jonharmrad17 jonharmrad15 jonharmrad16"
 
 
 # note that thickdisk1 is actually bad, so ignore it.
